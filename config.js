2b5da0755c750790098dd69fff048d0333b5e12d
SADIYA OFC <118267877+sadiya44@users.noreply.github.com>
2024-08-18 00:07:47 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put session id",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/3cef367ebfc7a45c10595.jpg"
+SESSION_ID: process.env.SESSION_ID || "bJZkDDSC#brT6mJXEbqDpYqe5ee2aHAus_pP4ircLu53zLPnckfc",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/3cef367ebfc7a45c10595.jpg",
+SUDO_NB: process.env.SUDO_NB || "94742195461"
 };
