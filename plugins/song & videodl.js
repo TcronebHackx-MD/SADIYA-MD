5880d7a20fd1472c302661820640f64ca1b76f0d
SADIYA OFC <118267877+sadiya44@users.noreply.github.com>
2024-08-18 00:13:51 +0530

    Update song & videodl.js

--- plugins/song & videodl.js
+++ plugins/song & videodl.js
@@ -10,7 +10,7 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-if(!q) return reply("please give me url...")
+if(!q) return reply("*❗කරුණාකර Link එකක් හො නමක් ලබා දෙන්න.*")
 const search = await yts(q)
 const data = search.videos[0]
 const url = data.url
@@ -54,7 +54,7 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-if(!q) return reply("please give me url...")
+if(!q) return reply("*❗කරුණාකර Link එකක් හො නමක් ලබා දෙන්න.*")
 const search = await ytv(q)
 const data = search.videos[0]
 const url = data.url
