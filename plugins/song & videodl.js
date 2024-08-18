f88472c4a7919ff0778e7b0865d1493b8a286c7b
SADIYA OFC <118267877+sadiya44@users.noreply.github.com>
2024-08-18 05:51:26 +0530

    Update song & videodl.js

--- plugins/song & videodl.js
+++ plugins/song & videodl.js
@@ -83,60 +83,7 @@ let downloadUrl = down.dl_url
 //send video
 await conn.sendMessage(from,{video:{url: downloadUrl},mimetype:"video/mp4"},{quoted:mek})
 await conn.sendMessage(from,{document:{url: downloadUrl},mimetype:"video/mp4",fileName:data.title + "mp4",caption:"MADE BY SADIYA-MD"},{quoted:mek})
-
-}catch(a){
-reply(`${a}`)
-}
-})let downloadUrl = down.dl_url
-
-//send audio
-await conn.sendMessage(from,{audio:{url: downloadUrl},mimetype:"audio/mpeg"},{quoted:mek})
-await conn.sendMessage(from,{document:{url: downloadUrl},mimetype:"audio/mpeg",fileName:data.title + "mp3",caption:"MADE BY SADIYA-MD"},{quoted:mek})
-}catch(e){
-reply(`${e}`)
-}
-})
-
-//===========video-dl===========
-
-cmd({
-    pattern: "video",
-    desc: "download videos",
-    category: "download",
-    filename: __filename
-},
-async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try{
-if(!q) return reply("*❗කරුණාකර Link එකක් හො නමක් ලබා දෙන්න.*")
-const search = await ytv(q)
-const data = search.videos[0]
-const url = data.url
-
-let desc = `*✨ SADIYA-MD VIDEO DOWNLOADER . .🌳*
-
-♦️ TITLE - ${data.title}
-
-♦️ VIEWS - ${data.views}
-
-♦️ DESCRIPTION - ${data.description}
-
-♦️ TIME - ${data.timestamp}
-
-♦️ AGO - ${data.ago}
-
-MADE BY SADIYA-MD
-`
-await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:mek});
-
-//download video
-
-let down = await fg.yta(url)  
-let downloadUrl = down.dl_url
-
-//send video
-await conn.sendMessage(from,{video:{url: downloadUrl},mimetype:"video/mp4"},{quoted:mek})
-await conn.sendMessage(from,{document:{url: downloadUrl},mimetype:"video/mp4",fileName:data.title + "mp4",caption:"MADE BY SADIYA-MD"},{quoted:mek})
-
+    
 }catch(a){
 reply(`${a}`)
 }
