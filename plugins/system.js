3cd041a34159ec89a6983aa1a07aff09650ec76b
SADIYA OFC <118267877+sadiya44@users.noreply.github.com>
2024-08-19 16:07:18 +0530

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -1,5 +1,6 @@
 const {cmd , commands} = require('../command')
 const os = require("os")
+const {runtime} = require('../lib/functions')
 cmd({
     pattern: "system",
     alias: ["status","botinfo"],
@@ -9,8 +10,12 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-  
-reply(`${status}`)
+let status = `*Uptime:*  ${runtime(process.uptime())}
+*Ram usage:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
+*HostName:* ${os.hostname()}
+*Owner:* Prabath Kumara & Your name
+`
+return reply(`${status}`)
 }catch(e){
 console.log(e)
 reply(`${e}`)
