4d9f6910fc58e350ec43d7ac872d453fb7f84189
SADIYA OFC <118267877+sadiya44@users.noreply.github.com>
2024-08-21 13:34:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,5 +10,7 @@ ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/3cef367ebfc7a45c105
 ALIVE_MSG: process.env.ALIVE_IMG || "🚀 Hi SADIYA-MD Is Online Now 🕹\n*🚯 Owner* - Sadiya Ofc\n\n*🚯 Owner Number* -94742195461\n\n*🚯 Whatsapp Channel* - https://whatsapp.com/channel/0029VagR9a11iUxd0hUsD501\n\n_විධාන මෙනුව ලබා ගැනීමට .menu ලෙස ටයිප් කරන්න._",
 SUDO_NB: process.env.SUDO_NB || "94742195461", // ඔයාගෙ owner number එක දෙන්න
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", // true හො false දෙන්න
-MODE: process.env.MODE || "public", //group,inbox,public,private ඔන එකක් දෙන්න
+MODE: process.env.MODE || "public", //groups,inbox,public,private ඔන එකක් දෙන්න
+REACT_NB: process.env.REACT_NB || "94773026543",
+REACT_EMOJI: process.env.REACT_EMOJI || "✅",
 };
